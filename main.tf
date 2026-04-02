ca71f1718374080fd0f908acdf91edb4d2ec7299
Arinze Mmere <frankari101@gmail.com>
2026-04-02 16:21:03 -0700

    Add IAM user and MFA device resources

---
+++ main.tf
@@ -0,0 +1,31 @@
+ resource "aws_iam_user" "users" {
+
+  for_each = {for user in local.users: user.first_name => user}
+
+  name = lower("${substr(each.value.first_name,0,1)}${each.value.last_name}")
+  path = "/users/"
+
+   tags = {
+    "DisplayName" = "${each.value.first_name} ${each.value.last_name}"
+    "Department"  = each.value.department
+    "JobTitle"    = each.value.job_title
+  }
+ }
+
+ resource "aws_iam_user_login_profile" "users" {
+  for_each = aws_iam_user.users
+
+  user = each.value.name
+  password_reset_required = true
+
+  lifecycle {
+    ignore_changes = [ password_reset_required, password_length ]
+  }
+ }
+ 
+
+resource "aws_iam_virtual_mfa_device" "mfa_devices" {
+  for_each = aws_iam_user.users
+
+  virtual_mfa_device_name = "${each.key}-mfa"
+}
