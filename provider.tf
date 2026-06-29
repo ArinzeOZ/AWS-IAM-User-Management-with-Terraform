ed2c7254daa3027ca9359e93dfc0be6004d25cd5
Arinze Mmere <frankari101@gmail.com>
2026-06-29 15:42:13 -0700

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,17 +1,17 @@
 terraform {
+  required_version = ">= 1.5.0"
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 6.52"
     }
   }
-
-  required_version = ">= 1.5.0"
 }
 
 provider "aws" {
   alias  = "primary"
-  region = var.primary_region
+  region = var.primary
 }
 
 provider "aws" {
